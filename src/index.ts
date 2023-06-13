712c26f4f1722bfa39ccfd0187439137b0cbc238
GandalfTheBlack16 <juanitonavarro90@gmail.com>
2023-06-13 23:40:46 +0200

    Added Pino logger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { HealthCheck } from './health.js'
+import { logger } from './shared/logger/Logger.js'
+import { pinoHttp } from 'pino-http'
 
 dotenv.config()
 
@@ -8,8 +10,10 @@ const port = process.env.PORT ?? 3000
 
 const app = express()
 
+app.use(pinoHttp({ logger, level: 'error' }))
+
 app.get('/health', HealthCheck)
 
 app.listen(port, () => {
-  console.log(`Application running on port ${port}`)
+  logger.info({ name: 'password-manager' }, `Application running on port ${port}`)
 })
--- src/user/domain/valueObjects/UserId.ts
+++ src/user/domain/valueObjects/UserId.ts
@@ -1,5 +1,5 @@
 import { validate } from 'uuid'
-import { IllegalArgsException } from '../../shared/exception/IllegalArgsException.js'
+import { IllegalArgsException } from '../../../shared/exception/IllegalArgsException.js'
 
 export class UserId {
   private readonly value: string

b953350292edd05ffa0779d5e78577e73167c169
GandalfTheBlack16 <juanitonavarro90@gmail.com>
2024-07-30 08:06:22 +0200

    Added extended JWT expiration feature

--- src/user/application/UserLogin.ts
+++ src/user/application/UserLogin.ts
@@ -1,6 +1,6 @@
 import { type UserRepository } from '../domain/repositories/UserRepository.js'
 import { generateJwt, verifyPassword } from '../../shared/application/crypto/CryptoUtils.js'
-import { type LoginUserResponseDto } from './user-types.js'
+import { type UserLoggerProps, type LoginUserResponseDto } from './user-types.js'
 
 export class UserLogin {
   constructor (private readonly userRepository: UserRepository) {}
@@ -10,21 +10,25 @@ export class UserLogin {
     message: 'Invalid username/email or password'
   }
 
-  async run ({ username, email, password }: { username: string, email: string, password: string }): Promise<LoginUserResponseDto> {
+  async run ({ username, email, password, keepLoggedIn }: UserLoggerProps): Promise<LoginUserResponseDto> {
     const user = !username
       ? await this.userRepository.getUserByEmail(email)
       : await this.userRepository.getUserByUsername(username)
+
     if (!user) {
       return this.UNAUTHORIZED_RESPONSE
     }
     if (!verifyPassword(password, user.getPassword)) {
       return this.UNAUTHORIZED_RESPONSE
     }
+
+    const expirationTime = keepLoggedIn ? process.env.JWT_EXTENDED_EXPIRATION_TIME : process.env.JWT_EXPIRATION_TIME
     const accessToken = generateJwt({
       id: user.getId,
       email: user.getEmail,
       username: user.getUsername
-    }, process.env.JWT_EXPIRATION_TIME)
+    }, expirationTime)
+
     return {
       status: 'Sucess',
       accessToken,
